d795f3973f39de43088a00d8d1e8636d80197ab6
slipperstree <slipperstree@gmail.com>
2015-05-03 22:54:34 +0900

    a

--- 01_rumen/04_button_led_color/prog.py
+++ 01_rumen/04_button_led_color/prog.py
@@ -15,7 +15,9 @@ RPi.GPIO.setup(R, RPi.GPIO.OUT)
 RPi.GPIO.setup(G, RPi.GPIO.OUT)
 RPi.GPIO.setup(B, RPi.GPIO.OUT)
 
-# 按钮连接的GPIO针脚的模式设置为信号输入模式
+# 按钮连接的GPIO针脚的模式设置为信号输入模式，同时默认拉高GPIO口电平，
+# 当GND没有被接通时，GPIO口处于高电平状态，取的的值为1
+# 注意到这是一个可选项，如果不在程序里面设置，通常的做法是通过一个上拉电阻连接到VCC上使之默认保持高电平
 RPi.GPIO.setup(btnR, RPi.GPIO.IN, pull_up_down=RPi.GPIO.PUD_UP)
 RPi.GPIO.setup(btnG, RPi.GPIO.IN, pull_up_down=RPi.GPIO.PUD_UP)
 RPi.GPIO.setup(btnB, RPi.GPIO.IN, pull_up_down=RPi.GPIO.PUD_UP)
