f50e305ca894521d0e483028ebe5d6a719183a74
slipperstree <slipperstree@gmail.com>
2015-05-10 14:36:22 +0900

    a

---
+++ 01_rumen/05_digit_led/prog.c
@@ -0,0 +1,221 @@
+#include <wiringPi.h>
+#include <unistd.h>
+
+// 定义单个数码管各段led对应的GPIO口
+// use "gpio readall" to get pin number in wiringPi(not BCM)
+// for pi2 the result is:
+/* 
+ +-----+-----+---------+------+---+---Pi 2---+---+------+---------+-----+-----+
+ | BCM | wPi |   Name  | Mode | V | Physical | V | Mode | Name    | wPi | BCM |
+ +-----+-----+---------+------+---+----++----+---+------+---------+-----+-----+
+ |     |     |    3.3v |      |   |  1 || 2  |   |      | 5v      |     |     |
+ |   2 |   8 |   SDA.1 |   IN | 1 |  3 || 4  |   |      | 5V      |     |     |
+ |   3 |   9 |   SCL.1 |   IN | 1 |  5 || 6  |   |      | 0v      |     |     |
+ |   4 |   7 | GPIO. 7 |   IN | 1 |  7 || 8  | 1 | ALT0 | TxD     | 15  | 14  |
+ |     |     |      0v |      |   |  9 || 10 | 1 | ALT0 | RxD     | 16  | 15  |
+ |  17 |   0 | GPIO. 0 |  OUT | 0 | 11 || 12 | 0 | IN   | GPIO. 1 | 1   | 18  |
+ |  27 |   2 | GPIO. 2 |   IN | 0 | 13 || 14 |   |      | 0v      |     |     |
+ |  22 |   3 | GPIO. 3 |   IN | 0 | 15 || 16 | 0 | IN   | GPIO. 4 | 4   | 23  |
+ |     |     |    3.3v |      |   | 17 || 18 | 0 | IN   | GPIO. 5 | 5   | 24  |
+ |  10 |  12 |    MOSI |   IN | 0 | 19 || 20 |   |      | 0v      |     |     |
+ |   9 |  13 |    MISO |   IN | 0 | 21 || 22 | 0 | IN   | GPIO. 6 | 6   | 25  |
+ |  11 |  14 |    SCLK |   IN | 0 | 23 || 24 | 1 | IN   | CE0     | 10  | 8   |
+ |     |     |      0v |      |   | 25 || 26 | 1 | IN   | CE1     | 11  | 7   |
+ |   0 |  30 |   SDA.0 |   IN | 1 | 27 || 28 | 1 | IN   | SCL.0   | 31  | 1   |
+ |   5 |  21 | GPIO.21 |   IN | 1 | 29 || 30 |   |      | 0v      |     |     |
+ |   6 |  22 | GPIO.22 |   IN | 1 | 31 || 32 | 0 | IN   | GPIO.26 | 26  | 12  |
+ |  13 |  23 | GPIO.23 |   IN | 0 | 33 || 34 |   |      | 0v      |     |     |
+ |  19 |  24 | GPIO.24 |   IN | 0 | 35 || 36 | 0 | IN   | GPIO.27 | 27  | 16  |
+ |  26 |  25 | GPIO.25 |   IN | 0 | 37 || 38 | 0 | IN   | GPIO.28 | 28  | 20  |
+ |     |     |      0v |      |   | 39 || 40 | 0 | IN   | GPIO.29 | 29  | 21  |
+ +-----+-----+---------+------+---+----++----+---+------+---------+-----+-----+
+ | BCM | wPi |   Name  | Mode | V | Physical | V | Mode | Name    | wPi | BCM |
+ +-----+-----+---------+------+---+---Pi 2---+---+------+---------+-----+-----+
+*/
+
+#define LED_A 25
+#define LED_B 24
+#define LED_C 23
+#define LED_D 22
+#define LED_E 21
+#define LED_F 14
+#define LED_G 13
+#define LED_DP 12
+
+// 定义1到4号数码管阳极对应的GPIO口
+#define DIGIT1 26
+#define DIGIT2 27
+#define DIGIT3 28
+#define DIGIT4 29
+
+// 定义按钮输入的GPIO口
+#define btn 2
+
+#define FALSE 0
+#define TRUE  1
+
+#define t 1000
+
+int i=0;
+
+// 指定no(1-4)号数码管显示数字num(0-9)，第三个参数是显示不显示小数点（1/0）
+void showDigit(int no, int num, int showDotPoint);
+
+int main (void) {
+  wiringPiSetup () ;
+
+  pinMode (LED_A, OUTPUT) ;
+  pinMode (LED_B, OUTPUT) ;
+  pinMode (LED_C, OUTPUT) ;
+  pinMode (LED_D, OUTPUT) ;
+  pinMode (LED_E, OUTPUT) ;
+  pinMode (LED_F, OUTPUT) ;
+  pinMode (LED_G, OUTPUT) ;
+  pinMode (LED_DP, OUTPUT) ;
+
+  pinMode (DIGIT1, OUTPUT) ;
+  pinMode (DIGIT2, OUTPUT) ;
+  pinMode (DIGIT3, OUTPUT) ;
+  pinMode (DIGIT4, OUTPUT) ;
+
+  pinMode (btn, INPUT) ;
+  pullUpDnControl (btn, PUD_UP) ;
+
+  digitalWrite (DIGIT1, HIGH) ;
+  digitalWrite (DIGIT2, HIGH) ;
+  digitalWrite (DIGIT3, HIGH) ;
+  digitalWrite (DIGIT4, HIGH) ;
+
+  for (i=0; i<1000; i++)
+  {
+    // 按钮按下时显示日期，否则显示时间
+    // 为了区别左右的数字，让第二个数码管的小数点显示出来
+    //（本来应该是一个冒号，我们这个数码管没有，就用小数点代替了）
+    if (digitalRead(btn) == HIGH) {
+      //usleep(t);
+      showDigit(1, 1, FALSE);
+      //usleep(t);
+      showDigit(2, 2, TRUE);
+      //usleep(t);
+      showDigit(3, 3, FALSE);
+      //usleep(t);
+      showDigit(4, 4, FALSE);
+    } else {
+      //usleep(t);
+      showDigit(1, 5, FALSE);
+      //usleep(t);
+      showDigit(2, 6, TRUE);
+      //usleep(t);
+      showDigit(3, 7, FALSE);
+      //usleep(t);
+      showDigit(4, 8, FALSE);
+    }
+  }
+  return 0 ;
+}
+
+void showDigit(int no, int num, int showDotPoint) {
+  // 先将正极拉低，关掉显示
+  digitalWrite (DIGIT1, LOW) ;
+  digitalWrite (DIGIT2, LOW) ;
+  digitalWrite (DIGIT3, LOW) ;
+  digitalWrite (DIGIT4, LOW) ;
+
+  if (num == 0) {
+    digitalWrite (LED_A, LOW) ;
+    digitalWrite (LED_B, LOW) ;
+    digitalWrite (LED_C, LOW) ;
+    digitalWrite (LED_D, LOW) ;
+    digitalWrite (LED_E, LOW) ;
+    digitalWrite (LED_F, LOW) ;
+    digitalWrite (LED_G, HIGH) ;
+  } else if (num == 1) {
+    digitalWrite (LED_A, HIGH) ;
+    digitalWrite (LED_B, LOW) ;
+    digitalWrite (LED_C, LOW) ;
+    digitalWrite (LED_D, HIGH) ;
+    digitalWrite (LED_E, HIGH) ;
+    digitalWrite (LED_F, HIGH) ;
+    digitalWrite (LED_G, HIGH) ;
+  } else if (num == 2) {
+    digitalWrite (LED_A, LOW) ;
+    digitalWrite (LED_B, LOW) ;
+    digitalWrite (LED_C, HIGH) ;
+    digitalWrite (LED_D, LOW) ;
+    digitalWrite (LED_E, LOW) ;
+    digitalWrite (LED_F, HIGH) ;
+    digitalWrite (LED_G, LOW) ;
+  } else if (num == 3) {
+    digitalWrite (LED_A, LOW) ;
+    digitalWrite (LED_B, LOW) ;
+    digitalWrite (LED_C, LOW) ;
+    digitalWrite (LED_D, LOW) ;
+    digitalWrite (LED_E, HIGH) ;
+    digitalWrite (LED_F, HIGH) ;
+    digitalWrite (LED_G, LOW) ;
+  } else if (num == 4) {
+    digitalWrite (LED_A, HIGH) ;
+    digitalWrite (LED_B, LOW) ;
+    digitalWrite (LED_C, LOW) ;
+    digitalWrite (LED_D, HIGH) ;
+    digitalWrite (LED_E, HIGH) ;
+    digitalWrite (LED_F, LOW) ;
+    digitalWrite (LED_G, LOW) ;
+  } else if (num == 5) {
+    digitalWrite (LED_A, LOW) ;
+    digitalWrite (LED_B, HIGH) ;
+    digitalWrite (LED_C, LOW) ;
+    digitalWrite (LED_D, LOW) ;
+    digitalWrite (LED_E, HIGH) ;
+    digitalWrite (LED_F, LOW) ;
+    digitalWrite (LED_G, LOW) ;
+  } else if (num == 6) {
+    digitalWrite (LED_A, LOW) ;
+    digitalWrite (LED_B, HIGH) ;
+    digitalWrite (LED_C, LOW) ;
+    digitalWrite (LED_D, LOW) ;
+    digitalWrite (LED_E, LOW) ;
+    digitalWrite (LED_F, LOW) ;
+    digitalWrite (LED_G, LOW) ;
+  } else if (num == 7) {
+    digitalWrite (LED_A, LOW) ;
+    digitalWrite (LED_B, LOW) ;
+    digitalWrite (LED_C, LOW) ;
+    digitalWrite (LED_D, HIGH) ;
+    digitalWrite (LED_E, HIGH) ;
+    digitalWrite (LED_F, HIGH) ;
+    digitalWrite (LED_G, HIGH) ;
+  } else if (num == 8) {
+    digitalWrite (LED_A, LOW) ;
+    digitalWrite (LED_B, LOW) ;
+    digitalWrite (LED_C, LOW) ;
+    digitalWrite (LED_D, LOW) ;
+    digitalWrite (LED_E, LOW) ;
+    digitalWrite (LED_F, LOW) ;
+    digitalWrite (LED_G, LOW) ;
+  } else if (num == 9) {
+    digitalWrite (LED_A, LOW) ;
+    digitalWrite (LED_B, LOW) ;
+    digitalWrite (LED_C, LOW) ;
+    digitalWrite (LED_D, LOW) ;
+    digitalWrite (LED_E, HIGH) ;
+    digitalWrite (LED_F, LOW) ;
+    digitalWrite (LED_G, LOW) ;
+  }
+  
+  if (showDotPoint == 1) {
+    digitalWrite (LED_DP, LOW) ;
+  } else {
+    digitalWrite (LED_DP, HIGH) ;
+  }
+
+  if (no == 1) {
+    digitalWrite (DIGIT1, HIGH) ;
+  } else if (no == 2) {
+    digitalWrite (DIGIT2, HIGH) ;
+  } else if (no == 3) {
+    digitalWrite (DIGIT3, HIGH) ;
+  } else if (no == 4) {
+    digitalWrite (DIGIT4, HIGH) ;
+  }
+}
