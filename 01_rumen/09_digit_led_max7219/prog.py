741dbd3ade082df8e38c04e6b12a0c07dcc59736
slipperstree <slipperstree@gmail.com>
2015-06-13 17:17:20 +0900

    a

--- 01_rumen/09_digit_led_max7219/prog.py
+++ 01_rumen/09_digit_led_max7219/prog.py
@@ -19,300 +19,137 @@ RPi.GPIO.setup(DIN, RPi.GPIO.OUT)
 RPi.GPIO.setup(LOAD, RPi.GPIO.OUT)
 RPi.GPIO.setup(CLK, RPi.GPIO.OUT)
 
+# 传输一个8位数
+def setByte(byteData):
+	# print ""
+	# print "S-----------setByte---------------:", hex(byteData)
+	for bit in range(0,8):
+		# 传入的数字从高位到低位依次判断是否为1，若为1则设置高电平，否则设置低电平
+		# 判断的方法是先向左移位，把要判断的位移动到最高位然后跟0x80（1000 0000）相与，
+		# 如果结果仍然是0x80（1000 0000）就表示最高位是1，否则最高位就是0
+		if ((byteData<<bit) & 0x80 == 0x80):
+			setBitData(True)
+			# print "1",
+		else:
+			setBitData(False)
+			# print "0",
+	# print ""
+	# print "E-----------setByte---------------"
+
 # 译码模式设置
 def setDecodeMode(mode = 4):
 	RPi.GPIO.output(LOAD, False)
 
 	# 指令寄存器地址设置：0xX9
 	# D15-D12:任意
-	setBitData(True)
-	setBitData(True)
-	setBitData(True)
-	setBitData(True)
 	# D11,10,9,8: 1,0,0,1
-	setBitData(True)
-	setBitData(False)
-	setBitData(False)
-	setBitData(True)
+	setByte(0x09)
 	
 	if (mode == 1) :
-		# TODO
-		print "TODO"
-		pass
+		# 所有数码管均不使用译码功能
+		# 指令代码: 0x00
+		setByte(0x00)
 	elif (mode == 2) :
-		# TODO
-		print "TODO"
-		pass
+		# 只对DIG0号数码管进行译码，其他数码管不使用译码功能
+		# 指令代码: 0x01
+		setByte(0x01)
 	elif (mode == 3) :
-		# TODO
-		print "TODO"
-		pass
+		# 对DIG0-3号数码管进行译码，其他数码管不使用译码功能
+		# 指令代码: 0x0F
+		setByte(0x0f)
 	elif (mode == 4) :
 		# 数码管7－0全部采用译码模式
-		# D7－D0： 1，1，1，1，1，1，1，1
-		setBitData(True)
-		setBitData(True)
-		setBitData(True)
-		setBitData(True)
-		setBitData(True)
-		setBitData(True)
-		setBitData(True)
-		setBitData(True)
+		# 指令代码: 0xFF
+		setByte(0xff)
 
 	RPi.GPIO.output(LOAD, True)
 
 # 亮度设置
-def setIntensity(mode = 15):
+def setIntensity(mode = 8):
 	RPi.GPIO.output(LOAD, False)
 
 	# 指令寄存器地址设置：0xXA
 	# D15-D12:任意
-	setBitData(True)
-	setBitData(True)
-	setBitData(True)
-	setBitData(True)
 	# D11,10,9,8: 1,0,1,0
-	setBitData(True)
-	setBitData(False)
-	setBitData(True)
-	setBitData(False)
+	setByte(0x0A)
 	
 	# 亮度从0到15共16个等级，指令的D3－D0就是数字0－15的二进制编码
 	# D7-D4:任意
-	setBitData(True)
-	setBitData(True)
-	setBitData(True)
-	setBitData(True)
-	
-	if (mode == 15) :
-		setBitData(False)
-		setBitData(True)
-		setBitData(False)
-		setBitData(False)
-	else :
-		# TODO
-		print "TODO"
-		pass
+	setByte(mode)
 
 	RPi.GPIO.output(LOAD, True)
 
-# 扫描显示位数设置
-def setScanLimit(mode = 4):
+# 扫描显示位数设置(0-7)
+def setScanLimit(mode = 7):
 	RPi.GPIO.output(LOAD, False)
 
 	# 指令寄存器地址设置：0xXB
 	# D15-D12:任意
-	setBitData(True)
-	setBitData(True)
-	setBitData(True)
-	setBitData(True)
 	# D11,10,9,8: 1,0,1,1
-	setBitData(True)
-	setBitData(False)
-	setBitData(True)
-	setBitData(True)
+	setByte(0x0B)
 	
 	# 扫描位数可设置0－7共8种选择，指令的D2－D0就是数字0－7的二进制编码
 	# D7-D3:任意
-	setBitData(True)
-	setBitData(True)
-	setBitData(True)
-	setBitData(True)
-	setBitData(True)
-	
-	if (mode == 4) :
-		setBitData(False)
-		setBitData(True)
-		setBitData(True)
-	else :
-		# TODO
-		print "TODO"
-		pass
+	# D2-D0:0-7的3位二进制编码
+	setByte(mode)
 
 	RPi.GPIO.output(LOAD, True)
 
 # 关断模式设置
+# mode: 1: 正常运行模式
+# mode: 0: 关断模式
 def setShutdownMode(mode = 1):
 	RPi.GPIO.output(LOAD, False)
 
 	# 指令寄存器地址设置：0xXC
 	# D15-D12:任意
-	setBitData(True)
-	setBitData(True)
-	setBitData(True)
-	setBitData(True)
 	# D11,10,9,8: 1,1,0,0
-	setBitData(True)
-	setBitData(True)
-	setBitData(False)
-	setBitData(False)
+	setByte(0x0C)
 	
 	# 关断模式可设置0－1共2种选择，设置D0即可
 	# D7-D1:任意
-	setBitData(True)
-	setBitData(True)
-	setBitData(True)
-	setBitData(True)
-	setBitData(True)
-	setBitData(True)
-	setBitData(True)
-	
-	if (mode == 1) :
-		# 正常运行模式
-		setBitData(True)
-	else :
-		# 关断模式
-		setBitData(False)
+	# D0:1: 正常运行模式 0: 关断模式
+	setByte(mode)
 
 	RPi.GPIO.output(LOAD, True)
 
 # 测试模式设置
+# mode=0: 正常运行模式
+# mode=1: 测试模式(全亮模式)
 def setDisplayTestMode(mode = 0):
 	RPi.GPIO.output(LOAD, False)
 
 	# 指令寄存器地址设置：0xXF
 	# D15-D12:任意
-	setBitData(True)
-	setBitData(True)
-	setBitData(True)
-	setBitData(True)
 	# D11,10,9,8: 1,1,1,1
-	setBitData(True)
-	setBitData(True)
-	setBitData(True)
-	setBitData(True)
+	setByte(0x0f)
 	
 	# 测试模式可设置0－1共2种选择，设置D0即可
 	# D7-D1:任意
-	setBitData(True)
-	setBitData(True)
-	setBitData(True)
-	setBitData(True)
-	setBitData(True)
-	setBitData(True)
-	setBitData(True)
-	
-	if (mode == 0) :
-		# 正常运行模式
-		setBitData(False)
-	else :
-		# 测试模式
-		setBitData(True)
+	# D0:0: 正常运行模式 1: 测试模式(全亮模式)
+	setByte(mode)
 
 	RPi.GPIO.output(LOAD, True)
 
-# 指定no(1-4)号数码管显示数字num(0-9)，第三个参数是显示不显示小数点（true/false）
+# 指定no(1-8)号数码管显示数字num(0-9)，第三个参数是显示不显示小数点（true/false）
 def showDigit(no, num, showDotPoint):
 
 	RPi.GPIO.output(LOAD, False)
 
-	if (no == 1) :
-		# 指令寄存器地址设置：0xX1
-		# D15-D12:任意
-		setBitData(True)
-		setBitData(True)
-		setBitData(True)
-		setBitData(True)
-		# D11-D8: 0,0,0,1
-		setBitData(False)
-		setBitData(False)
-		setBitData(False)
-		setBitData(True)
-	elif (no == 2) :
-		# 指令寄存器地址设置：0xX2
-		# D15-D12:任意
-		setBitData(True)
-		setBitData(True)
-		setBitData(True)
-		setBitData(True)
-		# D11-D8: 0,0,1,0
-		setBitData(False)
-		setBitData(False)
-		setBitData(True)
-		setBitData(False)
-	elif (no == 3) :
-		# 指令寄存器地址设置：0xX3
-		# D15-D12:任意
-		setBitData(True)
-		setBitData(True)
-		setBitData(True)
-		setBitData(True)
-		# D11-D8: 0,0,1,1
-		setBitData(False)
-		setBitData(False)
-		setBitData(True)
-		setBitData(True)
-	elif (no == 4) :
-		# 指令寄存器地址设置：0xX4
-		# D15-D12:任意
-		setBitData(True)
-		setBitData(True)
-		setBitData(True)
-		setBitData(True)
-		# D11-D8: 0,1,0,0
-		setBitData(False)
-		setBitData(True)
-		setBitData(False)
-		setBitData(False)
+	# 设置指令寄存器地址：0xX1-0xX8
+	# 格式：D15-D12:任意（我们这里设置0）
+	#       D11-D8: 1-8的4位二进制编码：例：1（0,0,0,1）
+	setByte(no)
 
-	# D7:显示小数点（1点亮）
-	setBitData(showDotPoint)
-	# D6-D4:任意
-	setBitData(True)
-	setBitData(True)
-	setBitData(True)
-
-	# D3-D0
-	if (num == 0) :
-		setBitData(False)
-		setBitData(False)
-		setBitData(False)
-		setBitData(False)
-	elif (num == 1) :
-		setBitData(False)
-		setBitData(False)
-		setBitData(False)
-		setBitData(True)
-	elif (num == 2) :
-		setBitData(False)
-		setBitData(False)
-		setBitData(True)
-		setBitData(False)
-	elif (num == 3) :
-		setBitData(False)
-		setBitData(False)
-		setBitData(True)
-		setBitData(True)
-	elif (num == 4) :
-		setBitData(False)
-		setBitData(True)
-		setBitData(False)
-		setBitData(False)
-	elif (num == 5) :
-		setBitData(False)
-		setBitData(True)
-		setBitData(False)
-		setBitData(True)
-	elif (num == 6) :
-		setBitData(False)
-		setBitData(True)
-		setBitData(True)
-		setBitData(False)
-	elif (num == 7) :
-		setBitData(False)
-		setBitData(True)
-		setBitData(True)
-		setBitData(True)
-	elif (num == 8) :
-		setBitData(True)
-		setBitData(False)
-		setBitData(False)
-		setBitData(False)
-	elif (num == 9) :
-		setBitData(True)
-		setBitData(False)
-		setBitData(False)
-		setBitData(True)
+	# 设置显示内容
+	# 格式：D7:显示小数点（1点亮）
+	#       D6-D4:任意（我们这里设置0）
+	#       D3-D0:数字0-9的4位二进制编码：例：2（0,0,1,0）
+	if (showDotPoint):
+		# 如果显示小数点则需要将数字的最高位（D7）设置为1(最高位跟1相或)
+		setByte(num | 0x80)
+	else:
+		setByte(num)
 
 	RPi.GPIO.output(LOAD, True)
 
@@ -322,19 +159,29 @@ def setBitData(data):
 	RPi.GPIO.output(CLK, True)
 
 try:
+	# 测试代码，亮度使用最低亮度，显示从0开始递增的数字。
+	# print "=====================setShutdownMode====================="
 	setShutdownMode()
+	# print "=====================setDecodeMode====================="
 	setDecodeMode()
-	setIntensity()
+	# print "=====================setIntensity====================="
+	setIntensity(0)
+	# print "=====================setScanLimit====================="
 	setScanLimit()
+	# print "=====================setDisplayTestMode====================="
 	setDisplayTestMode()
 
+	n=0
 	while True:
-		showDigit(1, int(time.strftime("%H",time.localtime(time.time()))) / 10, False)
-		showDigit(2, int(time.strftime("%H",time.localtime(time.time()))) % 10, True)
-		showDigit(3, int(time.strftime("%M",time.localtime(time.time()))) / 10, False)
-		showDigit(4, int(time.strftime("%M",time.localtime(time.time()))) % 10, False)
-		time.sleep(10)
-			
+		n=n+1
+		showDigit(8, int(n)%10, False)
+		showDigit(7, int(n/10)%10, False)
+		showDigit(6, int(n/100)%10, False)
+		showDigit(5, int(n/1000)%10, False)
+		showDigit(4, int(n/10000)%10, False)
+		showDigit(3, int(n/100000)%10, False)
+		showDigit(2, int(n/1000000)%10, False)
+		showDigit(1, int(n/10000000)%10, False)
 except KeyboardInterrupt:
 	pass
 
